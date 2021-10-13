00b0d6e3d38b250804549c7e811a893c31b6491d
simar-singh <simar.singh.r@gmail.com>
2021-10-13 18:12:08 -0400

    Created basic dashboard layout
    Created dashboard page with the layout and opportunities section with respective icons. Going to add modal functionality next

---
+++ scripts/chevron.js
@@ -0,0 +1,10 @@
+var down = false;
+
+window.onload = () => {
+  document.getElementById('chevron').src = down ? "./Resources/assets/chevron_down.svg" : "./Resources/assets/chevron_right.svg"
+  
+  document.getElementById('chevron').onclick = () => {
+    down = !down;
+    document.getElementById('chevron').src = down ? "./Resources/assets/chevron_down.svg" : "./Resources/assets/chevron_right.svg"
+  }
+}
